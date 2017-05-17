aaabc62e38c6aedcd772ae9f3227407b68206e94
h1416 <hieuvo1416@gmail.com>
2017-05-17 07:17:38 -0500

    Implementing custom pipe cont

--- app/products/product-filter.pipe.ts
+++ app/products/product-filter.pipe.ts
@@ -1,8 +1,12 @@
-import { PipeTransform } from "@angular/core";
+import { PipeTransform, Pipe } from "@angular/core";
 import { IProduct } from "./product";
+
+@Pipe({
+    name: 'productFilter'
+})
 
 export class ProductFilterPipe implements PipeTransform{
     transform(value: IProduct[], filterBy: string): IProduct[] {
-        throw new Error("Method not implemented.");
+        return;
     }
 }
